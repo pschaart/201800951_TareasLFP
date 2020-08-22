6297956ab4ffaa3f9141bed2a5993183ae8e57fe
pschaart <pschaart@gmail.com>
2020-08-22 00:44:10 -0600

    archivos correctos

--- Tarea 2/Lector_XML.py
+++ Tarea 2/Lector_XML.py
@@ -1,1 +1,12 @@
-import
+import xml.etree.ElementTree as ET
+
+def LeerDatos(Ruta):
+    Archivo = ET.parse(Ruta)
+    Registro = Archivo.getroot()
+
+    for x in Registro.findall('Estudiante'):
+        print(x.find('Nombre').text)
+        print(x.find('Carnet').text)
+        print(x.find('Carrera').text)
+        print(x.find('DPI').text)
+        print('')
--- Tarea 2/main.py
+++ Tarea 2/main.py
@@ -1,5 +1,6 @@
 import Lector_Json
 import Lecto_CSV
+import Lector_XML
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -16,5 +17,7 @@ if __name__ == '__main__':
     Lector_Json.CargarDatos('Datos.json')
     print('------------Archivo CSV --------------')
     Lecto_CSV.LeerDatos('Datos.csv')
+    print('----------- Archivo XML--------------')
+    Lector_XML.LeerDatos('Datos.xml')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
